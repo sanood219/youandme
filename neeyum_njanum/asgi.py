fd3a2d52f501f1e58d1998b2d13500562c25078e
sanood219 <sanuanu219@gmail.com>
2022-06-21 09:37:30 +0530

    ..

--- neeyum_njanum/asgi.py
+++ neeyum_njanum/asgi.py
@@ -24,6 +24,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 # })
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neeyum_njanum.settings")
 from chat import consumers
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -31,7 +32,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neeyum_njanum.settings")
+
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
