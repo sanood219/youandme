9439046cae707963635966c49ae00782d81c91b9
sanood219 <sanuanu219@gmail.com>
2022-06-21 09:16:28 +0530

    ..

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: daphne neeyum_njanum.asgi:application --port $PORT --bind 0.0.0.0 -v2
+chatworker: python manage.py runworker --settings=neeyum_njanum.settings -v2
--- neeyum_njanum/asgi.py
+++ neeyum_njanum/asgi.py
@@ -7,18 +7,47 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
+# import os
+# import chat.routing
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neeyum_njanum.settings')
+
+# application = ProtocolTypeRouter({
+#     'http': get_asgi_application(),
+#     'websocket':AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     )
+# })
+
 import os
-import chat.routing
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+from chat import consumers
 from channels.auth import AuthMiddlewareStack
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neeyum_njanum.settings')
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+from django.urls import path
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neeyum_njanum.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+# from chat.consumers import AdminChatConsumer, PublicChatConsumer
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket':AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+                path('chat/',consumers.ChatConsumer.as_asgi()),
+            ])
         )
-    )
+    ),
 })
